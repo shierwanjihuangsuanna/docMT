c65889d98ed84757e0c145d38601ad11ba67a3f0
“hoshizora” <shierwanjihuangsuanna@gmail.com>
2025-02-18 16:04:17 +0800

    测试tokenizer.model_max_length是多少

--- sft-test.py
+++ sft-test.py
@@ -2,7 +2,8 @@ import os.path
 
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
-from datasets import load_dataset
+from datasets import load_dataset,IterableDatasetDict
+
 
 # 从tim复制过来的
 PROMPT_DICT = {
@@ -15,36 +16,39 @@ PROMPT_DICT = {
         "### Request:\n{instruction}\n\n### Response:"
     ),
 }
-IGNORE_IDX=-100
-INPUT_MAX=400
+IGNORE_IDX = -100
+INPUT_MAX = 400
 DEFAULT_PAD_TOKEN = "[PAD]"
 DEFAULT_EOS_TOKEN = "</s>"
 DEFAULT_BOS_TOKEN = "<s>"
 DEFAULT_UNK_TOKEN = "<unk>"
 
+
 #从tim复制过来的
 
 def main():
-    # 确保使用 GPU（如果可用）
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"Using device: {device}")
-
-
     checkpoint_path = "./DeepSeek-R1-Distill-Llama-8B/"
-    train_data_path="./train_data/WMT23_1_20000.json"
-    output_dir="./checkpoints/test/"
+    train_file = "./train_data/WMT23_1_20000.json"
+    output_dir = "./checkpoints/test/"
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
-
-
-
-    # 加载数据集，用作 SFT 数据
-    dataset = load_dataset(path=train_data_path)
+    # 加载数据集
+    # copy from tim~ project
+    data_files = {"train": train_file}
+    extension ="json" ## 确定文件扩展名
+    raw_datasets = IterableDatasetDict()
+    raw_datasets["train"] = load_dataset(
+        extension,  # 数据集类型
+        data_files=data_files,
+        split="train",  ## 指定的加载分割
+        use_auth_token=True,
+        streaming=True )
+    # copy from tim~ project
 
     # 加载预训练的 Tokenizer 和模型
     tokenizer = AutoTokenizer.from_pretrained(checkpoint_path)
-    tokenizer.pad_token = tokenizer.eos_token  # 确保 GPT 模型有一个正确的 pad_token
+    tokenizer.pad_token = tokenizer.eos_token
     model = AutoModelForCausalLM.from_pretrained(checkpoint_path)
     print("tokenizer.model_max_length:{}".format(tokenizer.model_max_length))
     exit(0)
@@ -55,28 +59,24 @@ def main():
         # 从提示模板中获取提示 # 分别是有input的提示和没有input的提示
         prompt_input, prompt_no_input = PROMPT_DICT["prompt_input"], PROMPT_DICT["prompt_no_input"]
         ## 把数据内容插入到提示模板中
-        text = prompt_input.format_map(example) if example.get("input", "") != "" else prompt_no_input.format_map(example)
+        text = prompt_input.format_map(example) if example.get("input", "") != "" else prompt_no_input.format_map(
+            example)
 
-        example["text"] = text.replace("\\n", "\n") ##替换换行符
+        example["text"] = text.replace("\\n", "\n")  ##替换换行符
         example["labels"] = f"{example['output']}{tokenizer.eos_token}".replace("\\n", "\n")
         return example
-
 
     ## 【用datasets的map方法，对每一条数据执行replace function：
     # 把input插入prompt模板中，给output和badouts加上</s>符号【序列结束标记】
     replaced_dataset = dataset.map(replace_function)
 
-
-
-    def tokenize_function(examples): ## todo:修改该方法
+    def tokenize_function(examples):  ## todo:修改该方法
         # return tokenizer(examples["text"], padding="max_length", truncation=True, max_length=512)
 
         tokenize_output = {"input_ids": [], "attention_mask": [], "labels": []}  ## labels是用于计算损失的shift-labels？
 
         input_token = tokenizer(examples["text"], add_special_tokens=False)  # text列，所有的prompt，包含input和instruction的部分
         output_token = tokenizer(examples["labels"], add_special_tokens=False)  ## 好输出
-
-
 
         # 主要是要计算attention mask？
         for idx, (s, t) in enumerate(zip(input_token["input_ids"], output_token["input_ids"])):
@@ -89,7 +89,7 @@ def main():
             cur_labels = [IGNORE_IDX for _ in range(Len_s)] + t  ## promt的部分全都是IGNORE_IDX+真实的好输出
             cur_attention_mask = att_s + att_t
 
-            if cur_input_ids[-1] != tokenizer.eos_token_id: # 如果最后一个token不是eos则加上eos
+            if cur_input_ids[-1] != tokenizer.eos_token_id:  # 如果最后一个token不是eos则加上eos
                 cur_input_ids = cur_input_ids + [tokenizer.eos_token_id]
                 cur_labels = cur_labels + [tokenizer.eos_token_id]
                 cur_attention_mask = cur_attention_mask + [1]
@@ -101,19 +101,11 @@ def main():
 
         return tokenize_output
 
-
-
-
-
-
     tokenized_dataset = replaced_dataset.map(
         tokenize_function,
         batched=True,
         num_proc=4,
         remove_columns=["input", "output", "instruction"])
-
-
-
 
     # 定义训练参数
     training_args = TrainingArguments(
